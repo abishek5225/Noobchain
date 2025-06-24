0468a071836ac79b4fd0def821e9c21cdc13aac5
abishek5225 <abishekchaulagain5225@gmail.com>
2025-06-24 21:37:08 +0545

    third raw block

--- NoobChain.java
+++ NoobChain.java
@@ -3,10 +3,11 @@ public class NoobChain {
         Block firstBLock= new Block("Hello i'm the first block", "0");
         System.out.println("Hash for block 1: "+firstBLock.hash);
 
-        Block secondBlock = new Block("Hi i'm the second block", "0");
+        Block secondBlock = new Block("Hi i'm the second block", firstBLock.hash);
         System.out.println("Hash for block 2: "+secondBlock.hash);
 
-        
+        Block thirdBlock = new Block("Hello there i am third block",secondBlock.hash );
+        System.out.println("Hash for block 3: " +thirdBlock.hash);
 
 
     }
