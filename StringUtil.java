ae5be157543f5ada3e1b31a71ae2e5e1e992319d
abishek5225 <abishekchaulagain5225@gmail.com>
2025-06-24 21:22:01 +0545

    hexString appended

--- StringUtil.java
+++ StringUtil.java
@@ -1,7 +1,7 @@
 import java.security.MessageDigest;
 
 public class StringUtil {
-    public static void main(String input) {
+    public static String applySha256(String input) {
      try{
         MessageDigest digest= MessageDigest.getInstance("SHA-256");//CRYPTOGRAPHIC ALGORITHM "SHA-256"
 
@@ -10,10 +10,12 @@ public class StringUtil {
         for(int i=0;i<hash.length;i++){
             String hex= Integer.toHexString(0xff & hash[i]);
             if(hex.length()==1) hexString.append('0');
+            hexString.append(hex);
         }
+        return hexString.toString();
 
      }catch(Exception e){
-
+        throw new RuntimeException(e);
      }
     }
 }
