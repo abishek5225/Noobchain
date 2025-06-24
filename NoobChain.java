4f6a7abc336a1befba2a76488cb091f0a9c2b95f
abishek5225 <abishekchaulagain5225@gmail.com>
2025-06-24 21:32:26 +0545

    block 1 raw data added

---
+++ NoobChain.java
@@ -0,0 +1,8 @@
+public class NoobChain {
+    public static void main(String[] args) {
+        Block genesisBLock= new Block("Hello i'm the first block", "0");
+        System.out.println("Hash for block 1: "+genesisBLock.hash);
+
+        
+    }
+}
