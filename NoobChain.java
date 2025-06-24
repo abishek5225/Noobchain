d86f2b359b8f599b57830c21bd0ae5ec74bfbc35
abishek5225 <abishekchaulagain5225@gmail.com>
2025-06-24 21:34:31 +0545

    second raw block

--- NoobChain.java
+++ NoobChain.java
@@ -1,8 +1,13 @@
 public class NoobChain {
     public static void main(String[] args) {
-        Block genesisBLock= new Block("Hello i'm the first block", "0");
-        System.out.println("Hash for block 1: "+genesisBLock.hash);
+        Block firstBLock= new Block("Hello i'm the first block", "0");
+        System.out.println("Hash for block 1: "+firstBLock.hash);
+
+        Block secondBlock = new Block("Hi i'm the second block", "0");
+        System.out.println("Hash for block 2: "+secondBlock.hash);
 
         
+
+
     }
 }
