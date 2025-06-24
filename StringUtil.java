7c3605b120b1e55b6831a0c09db0cd705ff3a594
abishek5225 <abishekchaulagain5225@gmail.com>
2025-06-24 20:48:03 +0545

    loop for digital signature

--- StringUtil.java
+++ StringUtil.java
@@ -3,9 +3,15 @@ import java.security.MessageDigest;
 public class StringUtil {
     public static void main(String input) {
      try{
-        MessageDigest digest= MessageDigest.getInstance("SHA-256");
+        MessageDigest digest= MessageDigest.getInstance("SHA-256");//CRYPTOGRAPHIC ALGORITHM "SHA-256"
 
         byte[] hash=digest.digest(input.getBytes("UTF-8"));
+        StringBuffer hexString= new StringBuffer();//This will contain digital fingerprint
+        for(int i=0;i<hash.length;i++){
+            String hex= Integer.toHexString(0xff & hash[i]);
+            if(hex.length()==1) hexString.append('0');
+        }
+
      }   
     }
 }
