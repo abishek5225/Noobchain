2c0c7be8ed464e1ed5e2372ae66f865b146d2b6a
abishek5225 <abishekchaulagain5225@gmail.com>
2025-06-24 20:16:08 +0545

    block constructor created

--- Block.java
+++ Block.java
@@ -1,10 +1,17 @@
-
+import java.util.Date;
 
 public class Block{
 
     public String hash;
     public String previousHash;
     public String data;
-    public String timeStamp;
+    public long timeStamp;
+
+    //block constructor
+    public Block(String data, String previousHash){
+        this.data = data;
+        this.previousHash = previousHash;
+        this.timeStamp = new Date().getTime();
+    }
 
 }
