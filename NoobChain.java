00f7c8492c86f428cbc4750421ffef7e277335b6
abishek5225 <abishekchaulagain5225@gmail.com>
2025-06-24 22:03:09 +0545

    upated

--- NoobChain.java
+++ NoobChain.java
@@ -5,14 +5,8 @@ public class NoobChain {
 
     public static ArrayList<Block> blockchain= new ArrayList<Block>();
     public static void main(String[] args) {
-        Block firstBLock= new Block("Hello i'm the first block", "0");
-        System.out.println("Hash for block 1: "+firstBLock.hash);
-
-        Block secondBlock = new Block("Hi i'm the second block", firstBLock.hash);
-        System.out.println("Hash for block 2: "+secondBlock.hash);
-
-        Block thirdBlock = new Block("Hello there i am third block",secondBlock.hash );
-        System.out.println("Hash for block 3: " +thirdBlock.hash);
+        
+        blockchain.add(new Block("Hi i am first block", blockchain.get(blockchain.size()-1).hash));
 
 
     }
