6eed212ff62e697822122cc6ba63272789b7a6c4
abishek5225 <abishekchaulagain5225@gmail.com>
2025-06-25 13:13:07 +0545

    minor error fixed

--- StringUtil.java
+++ StringUtil.java
@@ -3,9 +3,13 @@ import java.security.MessageDigest;
 public class StringUtil {
     public static String applySha256(String input) {
      try{
-        MessageDigest digest= MessageDigest.getInstance("SHA-256");//CRYPTOGRAPHIC ALGORITHM "SHA-256"
+        MessageDigest digest= MessageDigest.getInstance("SHA-256");
+        /*MEssageDigest is a java class that helps to create hash value and  "SHA-256" is hashing logorithm*/
 
         byte[] hash=digest.digest(input.getBytes("UTF-8"));
+
+
+        
         StringBuffer hexString= new StringBuffer();//This will contain digital fingerprint
         for(int i=0;i<hash.length;i++){
             String hex= Integer.toHexString(0xff & hash[i]);
