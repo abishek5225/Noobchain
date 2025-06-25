da15466ee69780ed14b179914574fd390e00f698
abishek5225 <abishekchaulagain5225@gmail.com>
2025-06-25 13:15:28 +0545

    updated

--- NoobChain.java
+++ NoobChain.java
@@ -10,7 +10,7 @@ public class NoobChain {
         blockchain.add(new Block("I am second block",blockchain.get(blockchain.size()-1).hash));
         blockchain.add(new Block("I am third block", blockchain.get(blockchain.size()-1).hash));
 
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+       
 
 
 
