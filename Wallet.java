c961fa8c5ba9b1f88a70f8c6f62a3fb93bc2729e
abishek5225 <abishekchaulagain5225@gmail.com>
2025-07-02 23:15:04 +0545

    generate key pair method created

--- Wallet.java
+++ Wallet.java
@@ -3,4 +3,16 @@ import java.security.*;
 public class Wallet{
     public PrivateKey privateKey;
     public PublicKey publicKey;
+
+    public Wallet(){
+        generateKeyPair();
+    }
+
+    public void generateKeyPair(){
+        try{
+
+        }catch(Exception e){
+            throw new RuntimeException(e);
+        }
+    }
 }
